9ecd7d870210691c23b72c62aa0f7f11f5e9cd71
Steve Murigi <steve.murigi@student.moringaschool.com>
2024-07-17 08:20:43 -0500

    Changes

---
+++ index.js
@@ -0,0 +1,33 @@
+let shoppingList = [];
+
+document.addEventListener('DOMContentLoaded', () => {
+const itemInput = document.getElementById("itemInput");
+const addButton = document.getElementById("addButton");
+const listContainer = document.getElementById("shoppingList");
+const clearButton = document.getElementById("clearButton");
+
+addButton.addEventListener('click', () => {
+    const inputValue = itemInput.value;
+    addItemToList(inputValue);
+    itemInput.value = '';
+    });
+
+clearButton.addEventListener('click', () => {
+    shoppingList = [];
+    listContainer.innerHTML = '';
+    });
+
+function addItemToList(item){
+    const listItem = document.createElement('li');
+    listItem.textContent = item;
+    listContainer.appendChild(listItem);
+    shoppingList.push(item);
+}
+});
+
+
+
+
+
+
+
